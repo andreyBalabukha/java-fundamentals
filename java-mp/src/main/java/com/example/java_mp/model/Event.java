7c122ad15ea29da6f0445f18e5192c338f38f250
andreybalabukha <5344208@gmail.com>
2024-08-05 13:30:19 +0800

    impl caching

--- java-mp/src/main/java/com/example/java_mp/model/Event.java
+++ java-mp/src/main/java/com/example/java_mp/model/Event.java
@@ -3,11 +3,13 @@ package com.example.java_mp.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import java.time.ZonedDateTime;
 
 @Entity
 @Table(name = "events")
+@Cacheable
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
